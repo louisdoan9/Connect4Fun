086847a7fed0a644fb75a2c73c5b87ae8f725cc1
Louis Doan <louisdoan9@gmail.com>
2023-12-26 22:47:29 -0500

    Add function to check win condition

--- src/GameBoard.js
+++ src/GameBoard.js
@@ -23,6 +23,82 @@ function GameBoard() {
 		newBoard[i][column] = currentPlayer ? 'x' : 'o';
 		setCurrentPlayer(!currentPlayer);
 		setBoard(newBoard);
+		checkWin();
+	}
+
+	function checkEqual(a, b, c, d) {
+		if (a === b && b === c && c === d && a !== null) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	function checkWin() {
+		// check diagonal
+		let col = 0;
+		let row = 0;
+		while (row <= 5 && col <= 3) {
+			if (checkEqual(board[row][col], board[row][col + 1], board[row][col + 2], board[row][col + 3])) {
+				console.log('win');
+				return true;
+			} else {
+				col++;
+				if (col === 4) {
+					col = 0;
+					row++;
+				}
+			}
+		}
+		// check horizontal
+		col = 0;
+		row = 0;
+		while (row <= 2 && col <= 6) {
+			if (checkEqual(board[row][col], board[row + 1][col], board[row + 2][col], board[row + 3][col])) {
+				console.log('win 2');
+				return true;
+			} else {
+				col++;
+				if (col === 7) {
+					col = 0;
+					row++;
+				}
+			}
+		}
+		// check down right
+		col = 0;
+		row = 0;
+		while (row <= 2 && col <= 3) {
+			if (
+				checkEqual(board[row][col], board[row + 1][col + 1], board[row + 2][col + 2], board[row + 3][col + 3])
+			) {
+				console.log('win 3');
+				return true;
+			} else {
+				col++;
+				if (col === 4) {
+					col = 0;
+					row++;
+				}
+			}
+		}
+		// check down left
+		col = 3;
+		row = 0;
+		while (row <= 2 && col <= 6) {
+			if (
+				checkEqual(board[row][col], board[row + 1][col - 1], board[row + 2][col - 2], board[row + 3][col - 3])
+			) {
+				console.log('win 4');
+				return true;
+			} else {
+				col++;
+				if (col === 7) {
+					col = 3;
+					row++;
+				}
+			}
+		}
 	}
 
 	return (
